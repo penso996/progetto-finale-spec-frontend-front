60618012acd1b8bb32d257d26404760e5b207099
penso996 <penso.andrea@outlook.it>
2025-07-16 14:07:30 +0200

    working onf filters and query

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,22 @@ import { useEffect, useState } from "react";
 
 export default function HomePage() {
 
+    // useState to manage headphonesData
     const [headphoneData, setHeadphoneData] = useState([]);
 
-    async function fetchHeadphones() {
+    const [searchTitle, setSearchTitle] = useState("");
+
+    const categories = ["Over-Ear", "On-Ear", "In-Ear"];
+    const [searchCategory, setSearchCategory] = useState("");
+
+
+    const filteredHeadphoneData = headphoneData.filter(headphone =>
+        headphone.title.toLowerCase().includes(searchTitle.toLowerCase()) &&
+        (!searchCategory || headphone.category.toLowerCase() === searchCategory.toLowerCase())
+    );
+
+    // function to fetch headphonesData
+    async function fetchHeadphonesData() {
         try {
             const response = await fetch('http://localhost:3001/headphones');
             if (!response.ok) {
@@ -19,20 +32,41 @@ export default function HomePage() {
         }
     }
 
+    // useEffect
     useEffect(() => {
-        fetchHeadphones();
+        fetchHeadphonesData();
     }, []);
 
 
     // RENDER
     return (
         <main>
-            {headphoneData.map(headphone =>
+
+            <input type="text"
+                value={searchTitle}
+                onChange={e => setSearchTitle(e.target.value)}
+            />
+
+            <select
+                value={searchCategory}
+                onChange={e => setSearchCategory(e.target.value)}>
+
+                <option value="">--</option>
+                {categories.map((cat, ind) => (
+                    <option key={ind} value={cat}>
+                        {cat}
+                    </option>
+                ))}
+
+            </select>
+
+            {filteredHeadphoneData.map(headphone =>
                 <div key={headphone.id}>
-                    <h2>{headpgone.title}</h2>
+                    <h2>{headphone.title}</h2>
                     <h3>{headphone.category}</h3>
                 </div>
             )}
+
         </main>
     )
 }
