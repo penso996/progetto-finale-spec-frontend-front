75f3da7cdee0f402479e7f171151d4575713db43
penso996 <penso.andrea@outlook.it>
2025-07-18 16:22:17 +0200

    added favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -14,11 +14,32 @@ export default function Favorites() {
         headphonesData
     } = useContext(GlobalContext);
 
+    const favoritesHeadphones = headphonesData.filter(h => favorites.includes(h.id));
 
     // RENDER
     return (
         <main>
-            <h1>ciao</h1>
+            {/* headphones cards */}
+            <section className="headphones-section">
+                {favoritesHeadphones.length === 0 ? (
+                    <p className="not-found"><strong>No favorites headphones...</strong></p>
+                ) : (
+                    favoritesHeadphones.map(headphone => (
+                        <div className="headphones-card" key={headphone.id}>
+                            <p><strong>{headphone.title.toUpperCase()}</strong></p>
+                            <p>{headphone.category.toUpperCase()}</p>
+                            <NavLink to={`/headphones/${headphone.id}`}>
+                                <u>See complete spec sheets</u>
+                            </NavLink>
+                            <p onClick={() => toggleFavorite(headphone.id)}>
+                                {isFavorite(headphone.id) ?
+                                    <i className="fa-solid fa-heart-circle-minus" style={{ color: "var(--color-red)" }}></i> :
+                                    <i className="fa-solid fa-heart-circle-plus" style={{ color: "var(--color-red)" }}></i>}
+                            </p>
+                        </div>
+                    ))
+                )}
+            </section>
         </main>
     );
 }
