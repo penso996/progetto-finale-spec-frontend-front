4974caa54670718d61e08f2f1bbc7dd7cb2e6d22
penso996 <penso.andrea@outlook.it>
2025-07-19 17:50:17 +0200

    updated useCompare with function to reset

--- src/custom_hooks/useCompare.jsx
+++ src/custom_hooks/useCompare.jsx
@@ -1,14 +1,10 @@
 // Import hooks from React
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 
 export function useCompare() {
 
     const [compare, setCompare] = useState([]);
-
-    useEffect(() => {
-        console.log(compare)
-    }, [compare]);
 
     // add or remove an item by ID
     const toggleCompare = (id) => {
@@ -18,11 +14,14 @@ export function useCompare() {
                     ? prev.filter(i => i !== id)
                     : [...prev, id]
             )
-        }
+        };
     };
 
-    // check if ID is in favorites
+    // check if ID is in compare
     const isSelect = (id) => compare.includes(id);
 
-    return { compare, toggleCompare, isSelect };
+    // reset compare
+    const resetCompare = () => setCompare([]);
+
+    return { compare, toggleCompare, isSelect, resetCompare };
 }
